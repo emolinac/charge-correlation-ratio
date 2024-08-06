2b6d02b872cd002664c24fb2d73a66735e159c65
esteban <emolinac92@gmail.com>
2024-08-06 10:41:30 -0400

    mUpd: decay cut

--- src-decays/macro_print_rc_z_decaystringandalg.cpp
+++ src-decays/macro_print_rc_z_decaystringandalg.cpp
@@ -39,10 +39,10 @@ void macro_print_rc_z_decaystringandalg()
     TCut dh_kt_cut        = "dh_kt>0.597813&&dh_kt<0.848237";//"dh_kt>0.265&&dh_kt<0.885";
     TCut local_jet_pt_cut = "jet_pt>20&&jet_pt<100";
 
-    ntuple_decay->Project("hdecay_diffsign"    ,"nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==0&&(lh_motherid>22||lh_motherid<-10)&&(nlh_motherid>22||nlh_motherid<-10)");
+    ntuple_decay->Project("hdecay_diffsign"    ,"nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==0&&(lh_motherid>99||lh_motherid<-99)&&(nlh_motherid>99||nlh_motherid<-99)");
     ntuple_decay->Project("hstrbrk_diffsign"   ,"nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==0"+lh_motherid_cut+nlh_motherid_cut);
     ntuple_decay->Project("hstrbrkalg_diffsign","nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==0&&prob==1");
-    ntuple_decay->Project("hdecay_samesign"    ,"nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==1&&(lh_motherid>22||lh_motherid<-10)&&(nlh_motherid>22||nlh_motherid<-10)");
+    ntuple_decay->Project("hdecay_samesign"    ,"nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==1&&(lh_motherid>99||lh_motherid<-99)&&(nlh_motherid>99||nlh_motherid<-99)");
     ntuple_decay->Project("hstrbrk_samesign"   ,"nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==1"+lh_motherid_cut+nlh_motherid_cut);
     ntuple_decay->Project("hstrbrkalg_samesign","nlh_z",local_jet_pt_cut/*+dh_kt_cut*/+jet_cuts+trackmc_cuts+"eq_charge==1&&prob==1");
 
